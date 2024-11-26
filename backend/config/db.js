981497aa88d088b131de2b653dcb7e28db9c2de6
PRINCE KUMAR <152329144+Prince8574@users.noreply.github.com>
2024-11-26 11:20:49 -0500

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-export const connectDb =  async ()=>{
-    await mongoose.connect('mongodb+srv://prince8574:Prince8574@leadskills.eenmxvr.mongodb.net/').then(()=>console.log("!!!    Db Connected Suceessfully    |||"));
-
-}
